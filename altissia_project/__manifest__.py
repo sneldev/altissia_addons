1f4dc423e3af1fa0dac2c815bf24519e7f78a78a
Charles <charles.buysschaert@sneldev.com>
2018-02-25 22:10:07 +0100

    [project] altissia project: added module with cron

---
+++ altissia_project/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright Eezee-it
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Altissia Project",
+    "version": "0.1",
+    "author": "Eezee-it",
+    "category": "Project",
+    "website": "http://www.eezee-it.com",
+    "description": """
+    Customize the project module to meet the Altissia needs
+    """,
+    "depends": ['project'],
+    "data": ['data/project_task_cron.xml'],
+    "installable": True,
+}
