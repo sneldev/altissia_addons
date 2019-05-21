7078944705dfab16338d917327dfed668f8d1d98
Hergli Fares <fares.hergli@campion-tech.com>
2019-05-21 12:27:46 +0100

    2988: CRM : Sales report par email

--- altissia_crm/__manifest__.py
+++ altissia_crm/__manifest__.py
@@ -26,10 +26,13 @@
     "description": "Customize the CRM module to altissia's needs",
     "depends": ['base', 'crm', 'sale'],
     "data": [
+        "data/crm_sale_report_mail_template.xml",
+        "wizard/mail_compose_message_view.xml",
         "view/leads.xml",
         "view/opportunity.xml",
         "view/res_partner.xml",
         "view/lead_opportunity.xml",
+        "view/sales_views.xml",
     ],
     "installable": True,
     "active": True
