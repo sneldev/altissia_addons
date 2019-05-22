e5c3be8127a3d5d38dbf0e80fc9ebf88b11e4fea
sneldev <info@sneldev.com>
2019-05-22 14:38:07 +0200

    Update crm_lead.py

--- altissia_crm/crm_lead.py
+++ altissia_crm/crm_lead.py
@@ -87,3 +87,11 @@ class CrmLead(Model):
 
     website = Char('Website', size=64, help="Website of Partner or Company")
     lost_visible = Boolean(default=False ,compute='compute_lost_visible',store=False)
+    
+    
+class Message(models.Model):
+
+    _name = 'mail.message'
+    _inherit = 'mail.message'
+
+    body = fields.Html('Contents', default='', sanitize_style=False, strip_classes=True)    
