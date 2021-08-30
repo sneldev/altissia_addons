4e0aeb8f133cdffdc38353b03a713ff3e754219d
Gabriel <desiderio.gabriel@protonmail.com>
2021-08-30 15:01:12 +0200

    test address 2 footer

--- altissia_invoice_layout/models/bank.py
+++ altissia_invoice_layout/models/bank.py
@@ -8,5 +8,6 @@ class ResPartnerBank(models.Model):
                          help="Display this bank account on the footer of printed documents like invoices and sales orders.")
     clabe = fields.Char("Clabe",
                          help="Standardized bank key number")
+    bank_id = fields.Many2one('res.bank')
 
 
