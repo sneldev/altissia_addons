de6d3c9b4394a817b665be678aa362a52963c7be
Gabriel <desiderio.gabriel@protonmail.com>
2021-08-30 11:15:45 +0200

    manifest pour relancer serveur

--- profile_altissia/__manifest__.py
+++ profile_altissia/__manifest__.py
@@ -25,7 +25,7 @@
     "website": "http://www.eezee-it.com",
     "description": """Provide a profile for Altissia
     This module install the module\"Altissia Customization on CRM module\"""",
-    "depends": ['altissia_crm', 'altissia_project', 'mass_editing'],
+    "depends": ['altissia_crm', 'altissia_project', 'mass_editing','altissia_invoice_layout'],
     'data': [
         'data/mass_editing_group.xml',
         'views/mass_editing_view.xml',
